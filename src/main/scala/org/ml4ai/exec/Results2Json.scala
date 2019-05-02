9f58bf4238db87d6faf42b46ba8066a275d682f7
Enrique Noriega <enrique.noriega@gmail.com>
2019-05-02 16:46:10 -0700

    Some improvements to Results2Json

--- src/main/scala/org/ml4ai/exec/Results2Json.scala
+++ src/main/scala/org/ml4ai/exec/Results2Json.scala
@@ -6,9 +6,9 @@ import org.clulab.utils.Serializer
 import org.json4s.NoTypeHints
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.write
-import org.ml4ai.WHConfig
+import org.ml4ai.{WHConfig, WikiHopInstance}
 import org.ml4ai.exec.PathFinder.{NoPaths, Outcome, Successful, Unsuccessful}
-import org.ml4ai.utils.{md5Hash, using}
+import org.ml4ai.utils.{AnnotationsLoader, WikiHopParser, md5Hash, using}
 
 object Results2Json extends App {
 
@@ -20,11 +20,34 @@ object Results2Json extends App {
 
   implicit val formats = Serialization.formats(NoTypeHints)
 
+  private val annotationsPath = WHConfig.Files.annotationsFile
+
+  val loader = new AnnotationsLoader(annotationsPath, cache = false)
+
+  val successfulKeys = data.collect{
+    case (k, _:Successful) => k
+  }.toSet
+
+  val queries = WikiHopParser.trainingInstances.collect{
+    case WikiHopInstance(id, query, answer, _, _) if successfulKeys contains id =>
+      id -> (query.split(" ", 2).last, answer.get)
+  }.toMap
+
+  println(s"Data size: ${data.size}")
+
   using(new PrintWriter(outPath)){
     pw =>
-      pw.print(
-        write(
-          data.mapValues{
+      pw.print("[")
+
+      data.toSeq.zipWithIndex.foreach{
+        case ((k, v), ix) =>
+
+          if(ix % 100 == 0)
+            println(ix)
+
+          if(ix > 0)
+            pw.print(",")
+          pw.print(write((k,  v match {
             case Successful(paths) => Map("Successful" -> (paths map {
               elems =>
                 elems map {
@@ -32,15 +55,20 @@ object Results2Json extends App {
                     "Destination" -> e.destination,
                     "Attributions" -> e.attributions.map{
                       a =>
-                        Map("Doc" -> md5Hash(a.document), "Sentence" -> a.sentenceIx)
+                        val doc = loader(a.document)
+                        Map("Doc" -> a.document, "Sentence" -> a.sentenceIx, "SentenceText" -> doc.sentences(a.sentenceIx).getSentenceText)
                     })
                 }
-            }))
+            }),
+              "Start" -> queries(k)._1,
+              "End" -> queries(k)._2
+            )
             case NoPaths => "NoPath"
             case Unsuccessful(e) => Map("Error" -> e.toString)
-          }
-        )
-      )
+          })))
+      }
+
+      pw.print("]")
   }
 
 }
